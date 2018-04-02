b04bc5674b3331f783f0d5b87de365f8ee58c3ce
rperez2021 <rob.perez.flores@gmail.com>
2018-04-02 14:11:42 -0700

    Added keys

--- liri.js
+++ liri.js
@@ -1,1 +1,9 @@
+import { twitter } from "./keys.js";
+
 require("dotenv").config();
+const keys = require('./keys.js')
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
