a386fd6d5cd5037b4157cd817307d1f034bc9fd4
rperez2021 <rob.perez.flores@gmail.com>
2018-04-02 23:29:32 -0700

    Completed Twitter Section

--- liri.js
+++ liri.js
@@ -1,22 +1,28 @@
-import { twitter } from "./keys.js";
-
 require("dotenv").config();
 const keys = require('./keys.js')
 let [nope, morenope, thingtodo, input, ...idc] = process.argv
-
-var spotify = new Spotify(keys.spotify);
+var Twitter = require('twitter')
 var client = new Twitter(keys.twitter);
+var fs = require('fs')
+// var spotify = new spotify(keys.spotify);
 
-let dostuff = () => {
-    liri[thingtodo](input)
-}
+
+
 
 let liri = {
     my_tweets: (input) => {
-        var params = {screen_name: '@smartassrob'};
+        var params = {screen_name: '@smartassrob', count: 20};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
           if (!error) {
-            console.log(tweets);
+            console.log("Last 20 Tweets");
+            for (i = 0; i < tweets.length; i++) {
+                console.log(i+1 +". " + tweets[i].text)
+                fs.appendFile('./log.txt', i+1 +". " + tweets[i].text, 'utf8', (error) => {
+            if (error) throw err;
+            } )
+            }
+          } else {
+              console.log(error)
           }
         });
     },
@@ -30,3 +36,8 @@ let liri = {
 
     },
 }
+
+let dostuff = () => {
+    liri[thingtodo](input)
+}
+dostuff();
