31a6b54a9fbbf3facb9be58b7ac5e7cef273c480
rperez2021 <rob.perez.flores@gmail.com>
2018-04-02 21:49:16 -0700

    Worked on Liri's Functions

--- liri.js
+++ liri.js
@@ -2,8 +2,31 @@ import { twitter } from "./keys.js";
 
 require("dotenv").config();
 const keys = require('./keys.js')
+let [nope, morenope, thingtodo, input, ...idc] = process.argv
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+let dostuff = () => {
+    liri[thingtodo](input)
+}
 
+let liri = {
+    my_tweets: (input) => {
+        var params = {screen_name: '@smartassrob'};
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+          if (!error) {
+            console.log(tweets);
+          }
+        });
+    },
+    spotify_this_song: (input) => {
+
+    },
+    movie_this: (input) => {
+
+    },
+    do_what_it_says: (input) => {
+
+    },
+}
