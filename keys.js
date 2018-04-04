5fed1d284598eae967d7789c2a307fccaff4e310
rperez2021 <rob.perez.flores@gmail.com>
2018-04-04 10:27:06 -0700

    worked on OMDB and Spotify Sections

--- keys.js
+++ keys.js
@@ -1,17 +1,16 @@
 
-let twitter = {
+exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-let spotify = {
+exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-module.exports = {
-  twitter: twitter,
-  spotify: spotify, 
+exports.omdb = {
+  key: process.env.OMDB_KEY
 }
--- liri.js
+++ liri.js
@@ -1,39 +1,48 @@
 require("dotenv").config();
 const keys = require('./keys.js')
 let [nope, morenope, thingtodo, input, ...idc] = process.argv
-var Twitter = require('twitter')
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var omdb = require('omdb');
+var client2 = new spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var fs = require('fs')
-// var spotify = new spotify(keys.spotify);
 
 
 
 
 let liri = {
     my_tweets: (input) => {
-        var params = {screen_name: '@smartassrob', count: 20};
-        client.get('statuses/user_timeline', params, function(error, tweets, response) {
-          if (!error) {
-            console.log("Last 20 Tweets");
-            for (i = 0; i < tweets.length; i++) {
-                console.log(i+1 +". " + tweets[i].text)
-                fs.appendFile('./log.txt', i+1 +". " + tweets[i].text, 'utf8', (error) => {
-            if (error) throw err;
-            } )
+        var params = { screen_name: '@smartassrob', count: 20 };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error) {
+                console.log("Last 20 Tweets");
+                for (i = 0; i < tweets.length; i++) {
+                    console.log(i + 1 + ". " + tweets[i].text)
+                    fs.appendFile('./log.txt', i + 1 + ". " + tweets[i].text, 'utf8', (error) => {
+                        if (error) throw err;
+                    })
+                }
+            } else {
+                console.log(error)
             }
-          } else {
-              console.log(error)
-          }
         });
     },
     spotify_this_song: (input) => {
-
+        spotify.search({ type: 'track', query: search }, function (err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+            //Just testing if data is being pulled
+            console.log(data)
+        });
     },
     movie_this: (input) => {
-
+        //this is not compelted
     },
     do_what_it_says: (input) => {
-
+        // this is not completed
     },
 }
 
