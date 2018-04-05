0c19687db745cfd48a6b6a70d1b5ad019fd809f3
rperez2021 <rob.perez.flores@gmail.com>
2018-04-04 21:52:47 -0700

    OMDB and Spotify Updated, also changed the way the selections occur

--- liri.js
+++ liri.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 const keys = require('./keys.js')
-let [nope, morenope, thingtodo, input, ...idc] = process.argv
+let [nope, morenope, thingtodo, ...input] = process.argv
 var Twitter = require('twitter');
-var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var omdb = require('omdb');
-var client2 = new spotify(keys.spotify);
+let request = require("request")
+
 var client = new Twitter(keys.twitter);
 var fs = require('fs')
+input = input.join(" ")
 
 
 
 
 let liri = {
     my_tweets: (input) => {
-        var params = { screen_name: '@smartassrob', count: 20 };
+        console.log("hi")
+        var params = {
+            screen_name: '@smartassrob',
+            count: 20
+        };
         client.get('statuses/user_timeline', params, function (error, tweets, response) {
             if (!error) {
                 console.log("Last 20 Tweets");
@@ -28,25 +34,51 @@ let liri = {
             }
         });
     },
-    spotify_this_song: (input) => {
-        spotify.search({ type: 'track', query: search }, function (err, data) {
+    spotify_this: (input) => {
+        var spotify = new Spotify(keys.spotify);
+        spotify.search({
+            type: 'track',
+            query: input
+        }, function (err, data) {
             if (err) {
                 console.log('Error occurred: ' + err);
                 return;
             }
-            //Just testing if data is being pulled
-            console.log(data)
+            console.log(data.tracks.items[0].album.artists[0].name)
+            console.log(data.tracks.items[0].name)
+            console.log(data.tracks.items[0].external_urls.spotify)
+            console.log(data.tracks.items[0].album.name)
         });
     },
     movie_this: (input) => {
-        //this is not compelted
+        request('http://www.omdbapi.com/?apikey=c4448ae5&t=' + input, function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', JSON.parse(body).Released); // Print the HTML for the Google homepage.
+        });
     },
+
     do_what_it_says: (input) => {
         // this is not completed
+        // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+        // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+        // Feel free to change the text in that document to test out the feature for other commands.
     },
 }
 
-let dostuff = () => {
-    liri[thingtodo](input)
+// let dostuff = () => {
+//     liri[thingtodo](input)
+// }
+// dostuff();
+
+if (thingtodo === "my_tweets") { 
+    liri.my_tweets(input);
+} else if (thingtodo === "spotify_this") {
+    liri.spotify_this(input);
+} else if (thingtodo === "movie_this") {
+    liri.movie_this(input)
+} else if (thingtodo === "do_what_it_says") {
+    liri.do_what_it_says(input);
+} else {
+    console.log("that is not a valid command")
 }
-dostuff();
